4e080c1276c7665b99697116424da980be4ac963
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-07 16:22:58 -0600

    Setup server.js file
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const environment = process.env.NODE_ENV || 'development'
+const configuration = require('./knexfile')[environment]
+const database = require('knex')(configuration)
+
+const app = express()
+const port = 3000
+app.use(express.json())
+app.get('/', (request, response) => {
+  response.send('Hello World')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
