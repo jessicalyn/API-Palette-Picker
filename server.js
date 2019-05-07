f0306b595c59439c0270d0a536aac70e39e0f4a6
“Kimberly <kimberly.achkio@gmail.com>
2019-05-07 17:16:06 -0600

    Complete POST for new palette

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3000
 app.use(express.json())
 app.get('/', (request, response) => {
   response.send('Hello World')
-})
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
@@ -22,7 +22,7 @@ app.get('/api/v1/palettes', (request, response) => {
         error
       })
     })
-})
+});
 
 app.get('/api/v1/palettes/:id', (request, response) => {
   database('palettes').where('palette_id', request.params.id).select()
@@ -38,4 +38,24 @@ app.get('/api/v1/palettes/:id', (request, response) => {
         error
       })
     })
+});
+
+app.post('/api/v1/palettes', (request, response) => {
+  const newPalette = request.body
+
+  for(let requiredParameters of ['palette_name', 'project_id', 'color_1', 'color_2', 'color_3', 'color_4', 'color_5']) {
+    if (newPalette[requiredParameters] === undefined) {
+      response.status(422).json({ error: `Expected format: { palette_name: <String>}, project_id: <Integer>, color_1: <String>, color_2: <String>, color_3: <String>, color_4: <String>, color_5: <String>. You are missing a "${requiredParameter}"`})
+    } else {
+      database('palettes').insert(newPalette, 'palette_id')
+        .then(palette => {
+          response.status(201).json(palette)
+        })
+        .catch(error => {
+          response.status(500).json({
+            error
+        })
+      })
+    }
+  }
 })
