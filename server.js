ee03495675c0abad47b98ec93d78728da1e8b26a
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-07 16:51:54 -0600

    Complete GET for all projects and projects by id

--- server.js
+++ server.js
@@ -12,3 +12,28 @@ app.get('/', (request, response) => {
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+//Project Endpoints
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+    .then(projects => {
+      response.status(200).json(projects)
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
+
+app.get('/api/v1/projects/:id', (request, response) => {
+  const reqId = request.params.id
+  database('projects').where('project_id', reqId).select()
+    .then(project => {
+      if (project.length) {
+        response.status(200).json(project)
+      } else {
+        response.status(404).json({ error: `Could not find project with id ${reqId}`})
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
