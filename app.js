b69c21bf5879704ed9ff467357edd3a17fa445f9
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-09 07:59:36 -0600

    Complete PUT and DELETE for palettes by id

--- app.js
+++ app.js
@@ -59,35 +59,51 @@ app.post('/api/v1/palettes', (request, response) => {
   }
 });
 
-app.put('/api/v1/projects/:id', (request, response) => {
-  const update = request.body;
-  if(!update.project_name){
-    response.status(422).send({
-      error: `Missing required parameter. Expected format: { project_name: <String> }`
-    })
-  } else {
-    database('projects').where('project_id', request.params.id).select().update('project_name', update.project_name)
-      .then(project => {
-        if (project) {
+app.put('/api/palettes/:id', (request, response) => {
+  const updatedPalette = request.body
+  // okay no required params? or should we require name, just doesn't NEED to be updated?
+  database('palettes')
+    .where('palette_id', request.params.id)
+    .update({ 
+      palette_name: updatedPalette.palette_name || palette_name,
+      project_id: updatedPalette.project_id || project_id,
+      color_1: updatedPalette.color_1 || color_1,
+      color_2: updatedPalette.color_2 || color_2,
+      color_3: updatedPalette.color_3 || color_3,
+      color_4: updatedPalette.color_4 || color_4,
+      color_5: updatedPalette.color_5 || color_5,
+    }) 
+      .then(palette => {
+        if (palette.length) {
           response.status(200).json({
-            message: 'Project name successfully updated.'})
+            message: 'Palette successfully updated.'
+          })
         } else {
           response.status(404).json({
-            error: `Could not find a project with id ${request.params.id}.`
+            error: `Could not find a palette with id ${request.params.id}.`
           })
         }
       })
       .catch(error => {
         response.status(500).json({ error })
       })
-  }
 });
 
-app.put('/api/palettes/:id', (request, response) => {
-  const updatedPalette = request.body
-
-  
-})
+app.delete('/api/v1/palettes/:id', (request, response) => {
+  database('palettes').where('palette_id', request.params.id).del()
+    .then(res => {
+      if (res > 0) {
+        response.status(200).json(`Deleted palette with id ${request.params.id}`)
+      } else {
+        response.status(422).json({
+          error: `Could not find palette with id ${request.params.id}`
+        })
+      }
+    })
+    .catch(error => {
+      response.status(500).json({ error })
+    })
+});
   
 //Project Endpoints
 app.get('/api/v1/projects', (request, response) => {
