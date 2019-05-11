378dcb369ad69ccca28748e993a0a81dfde452a2
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-11 13:31:19 -0600

    Install and setup cors
    Co-authored-by: Kim Myers <kimberly.achkio@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require('./app')
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 3001)
 app.listen(app.get('port'), () => console.log(`Palette Picker listening on port ${app.get('port')}!`))
